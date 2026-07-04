e1cf37b1998ec070f6eaeacfe6de518d4d8ade82
Rasmus Ros <rasmus.ros@theca.com>
2026-07-04 19:59:59 +0200

    refactor: move the time-indexed cumulative emitter onto RelaxationBuilder

--- klause/src/commonTest/kotlin/com/eignex/klause/lp/relaxation/CumulativeTimeIndexedTest.kt
+++ klause/src/commonTest/kotlin/com/eignex/klause/lp/relaxation/CumulativeTimeIndexedTest.kt
@@ -24,7 +24,7 @@ import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 /** #453: the time-indexed `x_{i,t}` LP reformulation of the scheduling globals. */
-class CpToLpRelaxationCumulativeTimeIndexedTest {
+class CumulativeTimeIndexedTest {
 
     private val eps = 1e-6
 
