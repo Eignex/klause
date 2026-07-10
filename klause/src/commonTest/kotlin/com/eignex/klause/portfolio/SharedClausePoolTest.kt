59983f9f91e003bc47acae1f9c059b0598063ccc
Rasmus Ros <rasmus.ros@theca.com>
2026-07-10 14:24:35 +0200

    test: rename SharedClauseTest to SharedClausePoolTest to match its subject

--- klause/src/commonTest/kotlin/com/eignex/klause/portfolio/SharedClausePoolTest.kt
+++ klause/src/commonTest/kotlin/com/eignex/klause/portfolio/SharedClausePoolTest.kt
@@ -10,7 +10,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class SharedClauseTest {
+class SharedClausePoolTest {
 
     private fun twoIntVars(): Problem = Problem(
         numBoolVars = 0,
