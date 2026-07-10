a72593287c901c0830ea4141b8c53ab4baeb8b3e
Rasmus Ros <rasmus.ros@theca.com>
2026-07-10 22:48:32 +0200

    test: rename BreakCacheTest to prefix-match its file-under-test

--- klause/src/commonTest/kotlin/com/eignex/klause/localsearch/LocalSearchStateBreakCacheTest.kt
+++ klause/src/commonTest/kotlin/com/eignex/klause/localsearch/LocalSearchStateBreakCacheTest.kt
@@ -11,7 +11,7 @@ import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class BreakCacheTest {
+class LocalSearchStateBreakCacheTest {
 
     private fun naiveBreakScore(state: LocalSearchState, boolVar: Int): Int {
         var count = 0
