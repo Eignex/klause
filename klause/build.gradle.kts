135a7f2f46ee0b74af762514aa4a425a02c3c9cc
Rasmus Ros <rasmus.ros@theca.com>
2026-08-08 10:26:03 +0200

    chore: take kbuild 1.2.8 and move CI to JDK 25

--- klause/build.gradle.kts
+++ klause/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
-    id("com.eignex.kmp") version "1.2.7"
+    id("com.eignex.kmp") version "1.2.8"
     kotlin("plugin.serialization")
 }
 
