3806cc58eb5dc6925959bcf3aac9f118e8ca7ff0
Rasmus Ros <rasmus@eignex.com>
2026-08-26 09:45:11 +0200

    fix: wrap projection test support

--- klause/src/commonTest/kotlin/com/eignex/klause/factor/global/EngineProjectionTestSupport.kt
+++ klause/src/commonTest/kotlin/com/eignex/klause/factor/global/EngineProjectionTestSupport.kt
@@ -4,6 +4,11 @@ import com.eignex.klause.propagation.PropagationProblem
 import com.eignex.klause.propagation.Propagator
 import com.eignex.klause.solver.Problem
 
-internal val Problem.propagators: Array<out Propagator> get() = PropagationProblem(this).propagators
-internal val Problem.intOccurrences: Array<IntArray> get() = PropagationProblem(this).intOccurrences
-internal val Problem.nonIntEventWatcherIntOccurrences: Array<IntArray> get() = PropagationProblem(this).nonIntEventWatcherIntOccurrences
+internal val Problem.propagators: Array<out Propagator>
+    get() = PropagationProblem(this).propagators
+
+internal val Problem.intOccurrences: Array<IntArray>
+    get() = PropagationProblem(this).intOccurrences
+
+internal val Problem.nonIntEventWatcherIntOccurrences: Array<IntArray>
+    get() = PropagationProblem(this).nonIntEventWatcherIntOccurrences
--- klause/src/commonTest/kotlin/com/eignex/klause/solver/integration/EngineProjectionTestSupport.kt
+++ klause/src/commonTest/kotlin/com/eignex/klause/solver/integration/EngineProjectionTestSupport.kt
@@ -4,7 +4,14 @@ import com.eignex.klause.propagation.PropagationProblem
 import com.eignex.klause.propagation.Propagator
 import com.eignex.klause.solver.Problem
 
-internal val Problem.propagators: Array<out Propagator> get() = PropagationProblem(this).propagators
-internal val Problem.boolOccurrences: Array<IntArray> get() = PropagationProblem(this).boolOccurrences
-internal val Problem.intOccurrences: Array<IntArray> get() = PropagationProblem(this).intOccurrences
-internal val Problem.nonIntEventWatcherIntOccurrences: Array<IntArray> get() = PropagationProblem(this).nonIntEventWatcherIntOccurrences
+internal val Problem.propagators: Array<out Propagator>
+    get() = PropagationProblem(this).propagators
+
+internal val Problem.boolOccurrences: Array<IntArray>
+    get() = PropagationProblem(this).boolOccurrences
+
+internal val Problem.intOccurrences: Array<IntArray>
+    get() = PropagationProblem(this).intOccurrences
+
+internal val Problem.nonIntEventWatcherIntOccurrences: Array<IntArray>
+    get() = PropagationProblem(this).nonIntEventWatcherIntOccurrences
