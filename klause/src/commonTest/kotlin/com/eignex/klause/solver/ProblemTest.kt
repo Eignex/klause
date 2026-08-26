a1e667da20a5871bcc2b15a2ebfadebf9b2b8805
Rasmus Ros <rasmus@eignex.com>
2026-08-26 10:40:21 +0200

    fix: order problem test imports

--- klause/src/commonTest/kotlin/com/eignex/klause/solver/ProblemTest.kt
+++ klause/src/commonTest/kotlin/com/eignex/klause/solver/ProblemTest.kt
@@ -1,10 +1,10 @@
 package com.eignex.klause.solver
 
-import com.eignex.klause.ir.IntBounds
 import com.eignex.klause.factor.arithmetic.Linear
 import com.eignex.klause.factor.arithmetic.ReifiedRealLinear
 import com.eignex.klause.factor.bool.Clause
 import com.eignex.klause.factor.global.AllDifferent
+import com.eignex.klause.ir.IntBounds
 import com.eignex.klause.ir.LinearOp
 import com.eignex.klause.util.Bits
 import kotlin.test.Test
