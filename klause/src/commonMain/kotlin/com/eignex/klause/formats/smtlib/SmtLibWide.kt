9ac30b6fd4b1c301c2975498a0add704bac7468d
Rasmus Ros <rasmus@eignex.com>
2026-08-26 15:24:55 +0200

    refactor: move wide integer lowering out of formats

--- klause/src/commonMain/kotlin/com/eignex/klause/formats/smtlib/SmtLibWide.kt
+++ klause/src/commonMain/kotlin/com/eignex/klause/formats/smtlib/SmtLibWide.kt
@@ -1,7 +1,7 @@
 package com.eignex.klause.formats.smtlib
 
 import com.eignex.klause.factor.bool.Clause
-import com.eignex.klause.formats.wideIntColumns
+import com.eignex.klause.lowering.wideIntColumns
 import com.eignex.klause.lowering.IntComb
 import com.eignex.klause.lowering.LinComb
 import com.eignex.klause.lowering.WideLinComb
--- klause/src/commonMain/kotlin/com/eignex/klause/lowering/WideIntDigits.kt
+++ klause/src/commonMain/kotlin/com/eignex/klause/lowering/WideIntDigits.kt
@@ -1,4 +1,4 @@
-package com.eignex.klause.formats
+package com.eignex.klause.lowering
 
 import com.ionspin.kotlin.bignum.integer.BigInteger
 
