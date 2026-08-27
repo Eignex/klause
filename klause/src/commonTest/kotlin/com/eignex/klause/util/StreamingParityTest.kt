07bf7440323eef88b27abf8acdbd2dd8d5fea2fc
Rasmus Ros <rasmus@eignex.com>
2026-08-27 11:00:59 +0200

    fix: import XCSP streaming parser from lowering

--- klause/src/commonTest/kotlin/com/eignex/klause/util/StreamingParityTest.kt
+++ klause/src/commonTest/kotlin/com/eignex/klause/util/StreamingParityTest.kt
@@ -6,7 +6,7 @@ import com.eignex.klause.lowering.flatzinc.parseFlatZinc
 import com.eignex.klause.formats.mps.Mps
 import com.eignex.klause.formats.opb.Opb
 import com.eignex.klause.formats.smtlib.SmtLib
-import com.eignex.klause.formats.xcsp3.Xcsp3
+import com.eignex.klause.lowering.xcsp3.Xcsp3
 import com.eignex.klause.lowering.mps.toProblem
 import com.eignex.klause.lowering.opb.toProblem
 import com.eignex.klause.solver.Problem
