6cb54de37b32a223670829d08107fe4dab8f3fab
Rasmus Ros <rasmus@eignex.com>
2026-08-28 08:23:34 +0200

    test: sort ProblemSpec imports

--- klause/src/commonTest/kotlin/com/eignex/klause/theory/difference/DifferenceSearchComponentTest.kt
+++ klause/src/commonTest/kotlin/com/eignex/klause/theory/difference/DifferenceSearchComponentTest.kt
@@ -4,8 +4,8 @@ import com.eignex.klause.factor.arithmetic.Linear
 import com.eignex.klause.factor.arithmetic.ReifiedLinear
 import com.eignex.klause.ir.IntBounds
 import com.eignex.klause.ir.LinearOp
-import com.eignex.klause.solver.Factor
 import com.eignex.klause.ir.ProblemSpec
+import com.eignex.klause.solver.Factor
 import com.eignex.klause.solver.Sample
 import com.eignex.klause.solver.search.ComponentCheck
 import com.eignex.klause.solver.search.ComponentResult
