f473523c5e754ce8ba5ba5a393d3f026e582bd7c
Rasmus Ros <rasmus@eignex.com>
2026-08-28 07:08:01 +0200

    test: sort Problem imports

--- klause/src/jvmTest/kotlin/com/eignex/klause/portfolio/PortfolioTest.kt
+++ klause/src/jvmTest/kotlin/com/eignex/klause/portfolio/PortfolioTest.kt
@@ -9,6 +9,7 @@ import com.eignex.klause.factor.arithmetic.Linear
 import com.eignex.klause.factor.bool.Cardinality
 import com.eignex.klause.factor.bool.Clause
 import com.eignex.klause.ir.LinearOp
+import com.eignex.klause.ir.Problem
 import com.eignex.klause.localsearch.LocalSearchParams
 import com.eignex.klause.localsearch.LocalSearchSolver
 import com.eignex.klause.lp.bounding.LpConfig
@@ -18,7 +19,6 @@ import com.eignex.klause.propagation.bake
 import com.eignex.klause.solver.Factor
 import com.eignex.klause.solver.IntDomain
 import com.eignex.klause.solver.Lit
-import com.eignex.klause.ir.Problem
 import com.eignex.klause.solver.SolveResult
 import com.eignex.klause.solver.objective.LinearObjective
 import com.eignex.klause.solver.result.MinimizeResult
