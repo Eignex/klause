b949b3b93f48adbaa5239dbe6197d838e4f2f6ee
Rasmus Ros <rasmus@eignex.com>
2026-08-28 22:12:02 +0200

    fix: align boolean lp projection signatures

--- klause/src/commonMain/kotlin/com/eignex/klause/lp/BooleanFactorLpProjection.kt
+++ klause/src/commonMain/kotlin/com/eignex/klause/lp/BooleanFactorLpProjection.kt
@@ -5,15 +5,15 @@ import com.eignex.klause.factor.bool.Clause
 import com.eignex.klause.factor.bool.PseudoBoolean
 import com.eignex.klause.ir.LinearOp
 
-internal fun Clause.emitLpRelaxation(builder: RelaxationBuilder, factorId: Int) {
+internal fun Clause.emitLpRelaxation(builder: RelaxationBuilder) {
     builder.boolRow(literals, weights = null, op = LinearOp.GE, bound = 1L)
 }
 
-internal fun Cardinality.emitLpRelaxation(builder: RelaxationBuilder, factorId: Int) {
+internal fun Cardinality.emitLpRelaxation(builder: RelaxationBuilder) {
     builder.boolRow(literals, weights = null, op = LinearOp.GE, bound = min.toLong())
     builder.boolRow(literals, weights = null, op = LinearOp.LE, bound = max.toLong())
 }
 
-internal fun PseudoBoolean.emitLpRelaxation(builder: RelaxationBuilder, factorId: Int) {
+internal fun PseudoBoolean.emitLpRelaxation(builder: RelaxationBuilder) {
     builder.boolRow(literals, weights, relation, bound)
 }
